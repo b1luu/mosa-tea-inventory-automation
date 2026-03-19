55adc519c3ed8e9f65a105294becc00adedacd91
Brandon <b1luu@ucsd.edu>
2026-03-18 22:18:04 -0700

    explore

--- app/explore_catalog.py
+++ app/explore_catalog.py
@@ -3,6 +3,7 @@ from app.client import create_square_client
 
 from square.core.api_error import ApiError
 #returns a pager 
+client = create_square_client()
 try:
     response = client.catalog.list(types="ITEM")
     print(json.dumps(response.mode_dump(by_alias=True, exclude_none=True), indent=2))
