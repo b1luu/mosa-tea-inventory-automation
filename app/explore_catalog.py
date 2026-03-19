b26f9d57f9fc4984fafd3c793d2988ddb560a534
Brandon <b1luu@ucsd.edu>
2026-03-18 22:17:19 -0700

    explore

--- app/explore_catalog.py
+++ app/explore_catalog.py
@@ -4,7 +4,7 @@ from app.client import create_square_client
 from square.core.api_error import ApiError
 #returns a pager 
 try:
-    response = client.catalag.list(types="ITEM")
+    response = client.catalog.list(types="ITEM")
     print(json.dumps(response.mode_dump(by_alias=True, exclude_none=True), indent=2))
 except ApiError as error:
     print(error)
