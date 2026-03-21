c57e27086d65ad3841453c2b29e52a2aa80d10a2
Brandon <b1luu@ucsd.edu>
2026-03-20 22:19:43 -0700

    updated signature key for server.py and preparing logic flow

--- server.py
+++ server.py
@@ -1,21 +1,37 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Response
 from app.config import (
     get_square_webhook_signature_key,
     get_square_webhook_notification_url,
 )
 from square.utils.webhooks_helper import verify_signature
 
-#Minimal viable webhook receiver for learning 
+# Minimal viable Square webhook receiver for learning.
 
 app = FastAPI()
 
+
 @app.post("/webhook/square")
 async def square_webhook(request: Request):
+    signature_header = request.headers.get("x-square-hmacsha256-signature", "")
+    request_body = (await request.body()).decode("utf-8")
+
+    is_valid = verify_signature(
+        request_body=request_body,
+        signature_header=signature_header,
+        signature_key=get_square_webhook_signature_key(),
+        notification_url=get_square_webhook_notification_url(),
+    )
+
+    if not is_valid:
+        return Response(
+            content='{"error":"invalid signature"}',
+            media_type="application/json",
+            status_code=403,
+        )
+
     headers = dict(request.headers)
-    body = await request.body()
-    text = body.decode("utf-8")
 
     print(headers)
-    print(text)
+    print(request_body)
 
     return {"ok": True}
